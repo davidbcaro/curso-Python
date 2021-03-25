06952e923f6ddb69360db1dcf9fa472a760c4170
davidbcaro <davidbcaro@gmail.com>
2021-03-25 12:24:17 -0500

    chore: Update course

--- 05-operdorLogico.py
+++ 05-operdorLogico.py
@@ -1,18 +1,18 @@
-a=10
-b=15
-c=20
-resultado = ( (a<b) and (b<c) )
-print( a<b , " and ",b<c," : ",resultado )
+a = 10
+b = 15
+c = 20
+resultado = ((a<b) and (b<c))
+print( a<b , " and ", b<c," : ", resultado )
 # True and True 
 # resultado = True
-resultado = ( (a>b) and (b<c) )
-print( a>b , " and ",b<c," : ",resultado )
+resultado = ((a>b) and (b<c))
+print( a>b, " and ", b<c," : ", resultado )
 # Flase and True
 # resultado = False
-resultado = ( (a>b) or (b<c) )
-print( a>b , " or ",b<c," : ",resultado )
+resultado = ((a>b) or (b<c))
+print( a>b, " or ", b<c, " : ", resultado )
 # False or True
 # resultado = True
 resultadoNegado = not resultado
-print("Not ",resultado,":",resultadoNegado)
+print("Not ", resultado,":", resultadoNegado)
 # resultado = False 
--- 08-entradaDatos.py
+++ 08-entradaDatos.py
@@ -1,7 +1,7 @@
 nombre = input("Ingresa tu nombre: ")
 edad = int(input("Ingresa tu edad: "))
-print(f"Tu nombre es: {nombre} y tu edad es de {edad} años ")
-dias = edad * 365
+print(f"Tu nombre es: {nombre} y tu edad es de {edad} años")
+dias = edad*365
 print(f"Hace {dias:,} dias que naciste")
 
 # Ejercicios del 1 al 5 
