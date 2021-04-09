c7b530139bfc4998c81ee2ee59fe4c1d06a10959
davidbcaro <davidbcaro@gmail.com>
2021-04-09 09:56:10 -0500

    chore: Update course

--- 24-funciones2.py
+++ 24-funciones2.py
@@ -1,6 +1,14 @@
+def nombre(primer_nombre):
+    return primer_nombre 
+
+print(nombre("David"))
+
 def suma(a, b):
     resultado = a+b
     return resultado
 
-r = suma(5, 4)
+r = suma(6.4, 8)
 print(r)
+
+
+
